577faaf39c3e6837e2a2b95c76fe224ae15b13bd
Camilo Higuita <chiguitar@unal.edu.co>
2019-09-12 06:31:07 -0500

    all the fixes

--- src/fm/fmlist.cpp
+++ src/fm/fmlist.cpp
@@ -1,6 +1,6 @@
 /*
  * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2018  camilo <email>
+ * Copyright (C) 2018  camilo higuita <milo.h@aol.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
