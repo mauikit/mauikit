afcfbfcfb24ede1a350edcbe1ac5aac840b4ed1f
Camilo Higuita <chiguitar@unal.edu.co>
2020-02-23 12:26:28 -0500

    fmstatic can now get tag urls and filter them

--- src/utils/fmstatic.h
+++ src/utils/fmstatic.h
@@ -74,7 +74,7 @@ public slots:
 	static bool unFav(const QUrl &url);
 	static bool fav(const QUrl &url);
 	static bool toggleFav(const QUrl &url);
-	
+	static QList<QUrl> getTagUrls(const QString &tag, const QStringList &filters, const bool &strict = false);
 	static void bookmark(const QUrl &url);
 };
 
