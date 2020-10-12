6d1444a156f204688244e8885105456a8f03c0e2
Camilo Higuita <milo.h@aol.com>
2020-10-12 09:59:31 -0500

    add fonts type to fmh

--- src/utils/fmh.h
+++ src/utils/fmh.h
@@ -89,7 +89,7 @@ bool isIOS();
 /**
  * @brief The FILTER_TYPE enum
  */
-enum FILTER_TYPE : int { AUDIO, VIDEO, TEXT, IMAGE, DOCUMENT, COMPRESSED, NONE };
+enum FILTER_TYPE : int { AUDIO, VIDEO, TEXT, IMAGE, DOCUMENT, COMPRESSED, FONT, NONE };
 
 
 static const QStringList AUDIO_MIMETYPES = {"audio/mpeg", "audio/mp4", "audio/flac", "audio/ogg", "audio/wav"};
@@ -117,14 +117,15 @@ static const QStringList TEXT_MIMETYPES = {"text/markdown",
 static const QStringList IMAGE_MIMETYPES = {"image/bmp", "image/webp", "image/png", "image/gif", "image/jpeg", "image/web", "image/svg", "image/svg+xml"};
 static const QStringList DOCUMENT_MIMETYPES = {"application/pdf", "application/rtf", "application/doc", "application/odf"};
 static const QStringList COMPRESSED_MIMETYPES = {"application/x-compress","application/x-compressed", "application/x-xz-compressed-tar", "application/x-compressed-tar", "application/x-xz", "application/x-bzip", "application/x-gtar", "application/x-gzip", "application/zip"};
-
+static const QStringList FONT_MIMETYPES = {"font/ttf", "font/otf"};
 
 static const QMap<FILTER_TYPE, QStringList> SUPPORTED_MIMETYPES {{FILTER_TYPE::AUDIO, AUDIO_MIMETYPES},
                                                                  {FILTER_TYPE::VIDEO, VIDEO_MIMETYPES},
                                                                  {FILTER_TYPE::TEXT, TEXT_MIMETYPES},
                                                                  {FILTER_TYPE::IMAGE, IMAGE_MIMETYPES},
                                                                  {FILTER_TYPE::DOCUMENT, DOCUMENT_MIMETYPES},
-                                                                 {FMH::FILTER_TYPE::COMPRESSED, COMPRESSED_MIMETYPES}};
+                                                                 {FILTER_TYPE::FONT, FONT_MIMETYPES},
+                                                                 {FILTER_TYPE::COMPRESSED, COMPRESSED_MIMETYPES}};
 
 
 /**
@@ -150,7 +151,8 @@ static QHash<FILTER_TYPE, QStringList> FILTER_LIST = {{FILTER_TYPE::AUDIO, getMi
 {FILTER_TYPE::VIDEO, getMimeTypeSuffixes(FILTER_TYPE::VIDEO, [](QString suffix) -> QString { return "*." + suffix; })},
 {FILTER_TYPE::TEXT, getMimeTypeSuffixes(FILTER_TYPE::TEXT, [](QString suffix) -> QString { return "*." + suffix; })},
 {FILTER_TYPE::DOCUMENT, getMimeTypeSuffixes(FILTER_TYPE::DOCUMENT, [](QString suffix) -> QString { return "*." + suffix; })},
-{FILTER_TYPE::COMPRESSED, FMH::getMimeTypeSuffixes(FMH::FILTER_TYPE::COMPRESSED, [](QString suffix) -> QString { return "*." + suffix; })},
+{FILTER_TYPE::COMPRESSED, getMimeTypeSuffixes(FILTER_TYPE::COMPRESSED, [](QString suffix) -> QString { return "*." + suffix; })},
+{FILTER_TYPE::FONT, getMimeTypeSuffixes(FILTER_TYPE::FONT, [](QString suffix) -> QString { return "*." + suffix; })},
 {FILTER_TYPE::IMAGE, getMimeTypeSuffixes(FILTER_TYPE::IMAGE, [](QString suffix) -> QString { return "*." + suffix; })},
 {FILTER_TYPE::NONE, QStringList()}};
 
